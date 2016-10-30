c2d9a4fc1f4800b28ee527cd3c63f38be0798a89
yahier <xuziwang@gmail.com>
2016-10-30 18:57:05 +0800

    改善tab栏的选中颜色

--- app/src/main/java/com/yahier/learn/act/MainActivity.java
+++ app/src/main/java/com/yahier/learn/act/MainActivity.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.yahier.learn.R;
 import com.yahier.learn.adapter.CommonFragmentPagerAdapter;
@@ -22,8 +25,8 @@ public class MainActivity extends FragmentActivity {
 
     }
 
-    void initView(){
-        viewpager = (ViewPager)findViewById(R.id.viewpager);
+    void initView() {
+        viewpager = (ViewPager) findViewById(R.id.viewpager);
 
         ArrayList<Fragment> fragmentList = new ArrayList<>();
         Frament1Home fragment1 = new Frament1Home();
@@ -42,17 +45,62 @@ public class MainActivity extends FragmentActivity {
         viewpager.setAdapter(mAdapter);
 
 
+        LinearLayout linTvItem = (LinearLayout) findViewById(R.id.linTvItem);
+        final int childCount = linTvItem.getChildCount();
+        final TextView[] childs = new TextView[childCount];
+        for (int i = 0; i < childCount; i++) {
+            final int j = i;
+            childs[i] = (TextView) linTvItem.getChildAt(i);
+            childs[i].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    viewpager.setCurrentItem(j, true);
+                    for (int a = 0; a < childCount; a++) {
+                        if (j == a) {
+                            childs[a].setTextColor(getResources().getColor(R.color.theme_color));
+                        } else {
+                            childs[a].setTextColor(getResources().getColor(R.color.black));
+                        }
+                    }
 
+                }
+            });
+        }
+
+        viewpager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                for (int a = 0; a < childCount; a++) {
+                    if (position == a) {
+                        childs[a].setTextColor(getResources().getColor(R.color.theme_color));
+                    } else {
+                        childs[a].setTextColor(getResources().getColor(R.color.black));
+                    }
+                }
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
-    void test(){
+    void test() {
         getWindow().getDecorView().postDelayed(new Runnable() {
             @Override
             public void run() {
 
-                Intent intent = new Intent(MainActivity.this,PublishAct.class);
+                Intent intent = new Intent(MainActivity.this, PublishAct.class);
                 startActivity(intent);
             }
-        },2000);
+        }, 2000);
     }
 }
