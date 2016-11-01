97eca891e5d3ba04cf8a2a99e5e72f10163679ae
xuziwang <xuziwang@stbl.cc>
2016-11-01 14:48:05 +0800

    修改软件核心需求.

--- app/src/main/java/com/yahier/learn/adapter/Fragment1Adapter.java
+++ app/src/main/java/com/yahier/learn/adapter/Fragment1Adapter.java
@@ -2,10 +2,12 @@ package com.yahier.learn.adapter;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.view.ViewPager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.yahier.learn.R;
@@ -13,6 +15,7 @@ import com.yahier.learn.act.DetailAct;
 import com.yahier.learn.item.Skill;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 
 public class Fragment1Adapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
@@ -33,10 +36,10 @@ public class Fragment1Adapter extends RecyclerView.Adapter<RecyclerView.ViewHold
 
 
     @Override
-    public void onBindViewHolder(final RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder( RecyclerView.ViewHolder holder, int position) {
 
-
-        holder.itemView.setOnClickListener(new View.OnClickListener() {
+        ZhihuViewHolder ho = (ZhihuViewHolder)holder;
+        ho.itemView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(mContext, DetailAct.class);
@@ -44,17 +47,17 @@ public class Fragment1Adapter extends RecyclerView.Adapter<RecyclerView.ViewHold
             }
         });
 
+        ho.bind(position);
+
+
 
     }
-
-
 
 
     @Override
     public int getItemCount() {
-        return 10;
+        return 20;
     }
-
 
 
     private int getDataItemCount() {
@@ -68,15 +71,21 @@ public class Fragment1Adapter extends RecyclerView.Adapter<RecyclerView.ViewHold
     }
 
 
-
-    class ZhihuViewHolder extends RecyclerView.ViewHolder {
+    private class ZhihuViewHolder extends RecyclerView.ViewHolder {
         final TextView textView;
+        final ImageView img;
 
         ZhihuViewHolder(View itemView) {
             super(itemView);
             textView = (TextView) itemView.findViewById(R.id.tvTitle);
+            img = (ImageView) itemView.findViewById(R.id.img);
+        }
+
+        public void bind(int position){
+
 
         }
+
     }
 
 
